04c38aa7c80a39b2e994339112e086946002d72a
Renato Silveira <ren.oliv87@gmail.com>
2026-08-30 16:23:12 -0300

    pdfloader and ingestion done

---
+++ src/qa_bot_intelligent_assistant/ingestion/loader.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+from langchain_core.documents import Document
+from qa_bot_intelligent_assistant.ingestion.pdf_loader import load_pdf, load_pdfs_from_dir
+
+def load_document(file_path: str) -> list[Document]:
+    ext = Path(file_path).suffix.lower()
+
+    if ext == ".pdf":
+        return load_pdf(file_path)
+
+    raise ValueError(f"File type not yet supported: {ext}")
+
+def load_directory(directory: str) -> list[Document]:
+    return load_pdfs_from_dir(directory)
